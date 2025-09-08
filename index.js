1e90905c348fb6e39b3070f41e1231977a38936c
jaojuiii <jaojuigraphic2@gmail.com>
2025-09-08 15:50:44 +0700

    update

--- index.js
+++ index.js
@@ -2,56 +2,47 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 app.get("/api/:date", function (req, res) {
-  const date_string=req.params.date;
+  const date_string = req.params.date;
   const date = new Date(date_string);
-  
+
   if (date.toString() === "Invalid Date") {
-    const milliseconds_string=date_string;
+    const milliseconds_string = date_string;
     console.log(milliseconds_string);
     const date = new Date(Number(milliseconds_string));
-  res.json({
-    unix:date.getTime(),utc:date.toUTCString() 
-  })
-  if (date.toString() === "Invalid Date"){
-    return res.json({ error: "Invalid Date" });
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString(),
+    });
+    if (date.toString() === "Invalid Date") {
+      return res.json({ error: "Invalid Date" });
+    }
   }
-  }
   res.json({
-    unix:date.getTime(),utc:date.toUTCString() 
-  })
-});
-app.get("/api/:milliseconds", function (req, res) {
-  const milliseconds_string=req.params.milliseconds;
-  // const date_string=req.params.date;
-
-  const date = new Date(date_string);
-  // if (date.toString() === "Invalid Date") {
-  //   return res.json({ error: "Invalid Date" });
-  // }
-  res.json({
-    unix:milliseconds_string.getTime(),utc:date.toUTCString() 
-  })
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
 });
 
-app.get("/now", 
+app.get(
+  "/now",
   (req, res, next) => {
-    req.time = new Date(2015-12-25).toString();
+    req.time = new Date(2015 - 12 - 25).toString();
     next(); // ไปต่อยัง handler ถัดไป
   },
   (req, res) => {
@@ -59,16 +50,12 @@ app.get("/now",
   }
 );
 
-
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
-
-
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
